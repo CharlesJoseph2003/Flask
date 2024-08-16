91579e896f766d9eb61e1c60ca57fbe3114a42c9
CharlesJoseph2003 <105239968+CharlesJoseph2003@users.noreply.github.com>
2024-08-16 03:51:24 -0400

    halfway done

--- video.py
+++ video.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,21 +7,37 @@ api = Api(app)
 # Request parser allows you to parse and validate incoming request data and is good for handling data that
 # have been sent as JSONs
 video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of the video")
-video_put_args.add_argument("views", type=int, help="Views on the video")
-video_put_args.add_argument("likes", type=int, help="Likes on the video")
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True) #sends us a message saying that the name is required, new video will
+#not be created without the name string
+video_put_args.add_argument("views", type=int, help="Views on the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
 
 # dict of videos
 videos = {}
+def abort_if_video_id_doesnt_exist(video_id): #helper function
+    if video_id not in videos:
+        abort(404, message="Video ID is not valid") #will abort the operation and return a response back 
+
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(409, message="Video already exists with that ID")
 #hello
 class Video(Resource):
     def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
         return videos[video_id]
     
     def put(self, video_id):
+        abort_if_video_exists(video_id)
         args = video_put_args.parse_args()  # will automatically get all the arguments and if they are not sent, then it will send back an error message
-        videos[video_id] = args
-        return {video_id: args}
+        videos[video_id] = args #adds the video id as the key to the dict and the args as the value
+        # return {video_id: args}
+        return videos[video_id], 201 #sends status code of 201 to show that video was created, and returns the dict 
+    
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        return '', 204 
+
     
 api.add_resource(Video, "/video/<int:video_id>")
 
--- video_test.py
+++ video_test.py
@@ -2,5 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 headers = {"Content-Type": "application/json"}
-response = requests.put(BASE + "video/1", json={"likes": 10}, headers=headers)
+# response = requests.put(BASE + "video/1", json={"likes": 10, "name": "Charles", "views": 100}, headers=headers)
+# print(response.json())
+# input() #waits for the user to press enter to input some data, acts like a pause
+response = requests.get(BASE + 'video/6') #if a video_id that isn't in the videos dict is called, it will return the abort message  
 print(response.json())
