3b236366d4888896a1d0fe0368a6b36778acaf4e
CharlesJoseph2003 <105239968+CharlesJoseph2003@users.noreply.github.com>
2024-08-16 01:49:57 -0400

    initial commit

---
+++ practice/main.py
@@ -0,0 +1,30 @@
+import requests
+
+BASE = "http://127.0.0.1:5000/"
+headers = {"Content-Type": "application/json"}
+response = requests.put(BASE + "video/1", json={"likes": 10}, headers=headers)
+print(response.json())
+
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+# hello
+
+# list of names
+names = {"tim": {"age": 19, "gender": "male"},
+         "bill": {"age": 70, "gender": "male"}}
+
+class HelloWorld(Resource):
+    # Resource represents collection of entities that can be accessed and manipulated using GET POST PUT and DELETE requests
+    def get(self, name):
+        return names[name]
+
+    # Iterating through the dict of names and getting the values associated with the key
+    # Changed from set to dictionary
+    # Information have to be serializable, in json format
+    # want to return json serializable objects so have to return dictionaries in general
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
